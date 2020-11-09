03a4358bf4c1a89c26ddd7b43242eefd08ccfccb
Daniel Siegenthaler <daniel.siegenthaler@students.bfh.ch>
2020-11-09 14:41:21 +0100

    Update hallo

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'Dänu Sigi'?></h1>
+        <h1>Hallo <?php echo 'Dänu'?></h1>
         <p class="lead">APPUiO Example Dockerfile PHP</p>
       </div>
 
